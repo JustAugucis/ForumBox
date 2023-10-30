41d93d528b96555cb3f336c5b2af04962eb112f0
JustAugucis <62718304+JustAugucis@users.noreply.github.com>
2023-10-30 10:39:19 +0100

    Fixed migration
    Need to set up persistance next

--- FileData/DAOs/UserFileDao.cs
+++ FileData/DAOs/UserFileDao.cs
@@ -15,14 +15,7 @@ public class UserFileDao : IUserDao
 
     public Task<User> CreateAsync(User user)
     {
-        int userId = 1;
-        if (context.Users.Any())
-        {
-            userId = context.Users.Max(u => u.Id);
-            userId++;
-        }
-
-        user.Id = userId;
+        
 
         context.Users.Add(user);
         context.SaveChanges();
@@ -33,17 +26,11 @@ public class UserFileDao : IUserDao
     public Task<User?> GetByUsernameAsync(string userName)
     {
         User? existing = context.Users.FirstOrDefault(u =>
-            u.UserName.Equals(userName, StringComparison.OrdinalIgnoreCase)
+            u.Username.Equals(userName, StringComparison.OrdinalIgnoreCase)
         );
         return Task.FromResult(existing);
     }
 
-    public Task<User?> GetByIdAsync(int dtoOwnerId)
-    {
-        User? existing = context.Users.FirstOrDefault(u =>
-            u.Id.Equals(dtoOwnerId)
-        );
-        return Task.FromResult(existing);
-    }
+ 
     
 }
